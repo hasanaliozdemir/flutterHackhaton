d1fc4d1bb0c66fac317fcfbd43c926048d68963d
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-12 18:04:38 +0300

    homepage done

--- flutter_app/lib/view/homepage/homepage.dart
+++ flutter_app/lib/view/homepage/homepage.dart
@@ -1,21 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/colors.dart';
 import 'package:flutter_app/core/components/bottom_bar.dart';
+import 'package:flutter_app/view/homepage/send_question_card.dart';
+import 'package:flutter_app/view/homepage/target_question_card.dart';
+import 'package:flutter_app/view/homepage/welcom_card.dart';
+
+import 'chose_question_or_lesson.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int money = 100;
+  String userName = "Hasan";
+  int solved = 50;
+  int target = 500;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text("Home Page"),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(image: DecorationImage(
+              image: AssetImage("assets/pattern.png"),
+              opacity: 0.1,
+              fit: BoxFit.cover
+            )),
+          ),
+          Padding(padding: const EdgeInsets.all(16), child: _buildBody()),
+        ],
       ),
-      bottomNavigationBar: CustomBottomBar(index: 0),
+      bottomNavigationBar: const CustomBottomBar(index: 0),
     );
   }
+
+  Column _buildBody() {
+    return Column(
+        children: [
+          const SizedBox(
+            height: 68,
+          ),
+          WelcomeCard(userName: userName, money: money),
+          const SizedBox(
+            height: 24,
+          ),
+          TargetQuestionCard(solved: solved, target: target),
+          const SizedBox(
+            height: 24,
+          ),
+          const ChoseQuestonOrLesson(),
+          const SizedBox(
+            height: 24,
+          ),
+          const SenQuestionCard()
+        ],
+      );
+  }
 }
+
