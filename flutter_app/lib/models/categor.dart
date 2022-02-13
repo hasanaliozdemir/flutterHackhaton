4db2b28a35b5748a76ce580d7ec6d0b077005209
hasanaliozdemir <68190862+hasanaliozdemir@users.noreply.github.com>
2022-02-13 05:28:47 +0300

    category model

--- flutter_app/lib/models/categor.dart
+++ flutter_app/lib/models/categor.dart
@@ -1,6 +1,7 @@
 class Category {
   final String name;
   final double percent;
+  final List<String> subTopics;
 
-  Category(this.name, this.percent);
+  Category(this.name, this.percent, this.subTopics);
 }
